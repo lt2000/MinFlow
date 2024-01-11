fb61238dec8e0afcb33373d6ba4bba04c3b3f845
lt2000 <1378520056@qq.com>
2024-01-11 23:46:56 +0800

    fix AEC issues

--- scripts/create_image.bash
+++ scripts/create_image.bash
@@ -1,5 +1,5 @@
-docker image rm minflow_base
-docker build --no-cache -t minflow_base ~/minflow/src/base
+# docker image rm minflow_base
+# docker build --no-cache -t minflow_base ~/minflow/src/base
 
 docker image rm workflow_async_base
 docker build --no-cache -t workflow_async_base ~/minflow/src/container
