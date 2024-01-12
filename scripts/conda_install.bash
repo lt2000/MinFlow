93cd8debc6d97063dac161a40a9a468e65442cfb
lt2000 <1378520056@qq.com>
2024-01-12 16:21:20 +0800

    update conda install script

--- scripts/conda_install.bash
+++ scripts/conda_install.bash
@@ -1,22 +1,34 @@
-# Set Miniconda version and Python version
-PYTHON_VERSION="3.9"
+#!/bin/zsh
 
-# Download Miniconda installer
-wget https://repo.anaconda.com/archive/Anaconda3-2022.05-Linux-x86_64.sh -O anaconda.sh
+# Define the URL and installation directory for the Anaconda installation file
+anaconda_installer_url="https://repo.anaconda.com/archive/Anaconda3-2022.05-Linux-x86_64.sh"
+install_dir="$HOME/anaconda3"
 
-# Install Miniconda
-bash anaconda.sh -b -p $HOME/anaconda3
-rm anaconda.sh
+# Download the Anaconda installation file
+echo "Downloading Anaconda installer..."
+curl -O $anaconda_installer_url
 
-# Initialize conda and update
-conda init
-source ~/.zshrc
+# Run the Anaconda installation process
+echo "Installing Anaconda..."
+bash Anaconda3-2022.05-Linux-x86_64.sh -b -p $install_dir
 
-# Create a new Python environment
-conda create --yes --name minflow python=$PYTHON_VERSION
+# Add Anaconda to the PATH
+echo "Adding Anaconda to PATH..."
+echo "export PATH=$HOME/anaconda3/bin:\$PATH" >> $HOME/.zshrc
+source $HOME/.zshrc
+conda init zsh
+source $HOME/.zshrc
+
+# Remove the downloaded installation file
+echo "Cleaning up..."
+rm Anaconda3-2022.05-Linux-x86_64.sh
+
+echo "Anaconda has been installed successfully. Please restart your shell or run 'source $HOME/.zshrc'."
+
+# Create and activate the conda environment
+echo "Creating and activating 'minflow' environment..."
+conda create --yes --name minflow python=3.9
+source $HOME/.zshrc
 conda activate minflow
 
-# Display conda info
-conda info
-
-echo "Anaconda and Python $PYTHON_VERSION have been successfully installed. You are now in a new conda environment named 'minflow'."
+echo "minflow environment has been created and activated. You can start using it!"
